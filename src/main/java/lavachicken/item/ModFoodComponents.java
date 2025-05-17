3688c62e36bbbad3c0d2b17d105a3317e028cb8c
ikon1024 <ikon1024@kvsky.email>
2025-05-17 14:53:17 -0700

    Update Status Effects

--- src/main/java/lavachicken/item/ModFoodComponents.java
+++ src/main/java/lavachicken/item/ModFoodComponents.java
@@ -9,11 +9,12 @@ public class ModFoodComponents {
             .nutrition(6)
             .saturationModifier(1.0f)
             .alwaysEdible()
-            .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 1000, 0), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 600, 0), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 200, 0), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 1000, 0), 1.0f)
             .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 1000, 4), 1.0f)
             .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 1000, 0), 1.0f)
             .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 1000, 1), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 2), 1.0f)
             .snack()
             .build();
 }
